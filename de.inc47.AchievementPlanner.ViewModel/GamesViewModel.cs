6d5e1845607178ab423c2451bc012b9468f95408
Christoph Küpker <christoph.kuepker@googlemail.com>
2020-10-17 23:51:00 +0200

    Added command to update games

--- de.inc47.AchievementPlanner.ViewModel/GamesViewModel.cs
+++ de.inc47.AchievementPlanner.ViewModel/GamesViewModel.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Windows.Input;
 using de.inc47.AchievementPlanner.Model;
 
 namespace de.inc47.AchievementPlanner.ViewModel
 {
+  /// <summary>
+  /// ViewModel for the list of games tab.
+  /// </summary>
   public class GamesViewModel : ViewModelBase, IGamesViewModel
   {
     private readonly ObservableCollection<IGame> _games;
@@ -17,6 +21,11 @@ namespace de.inc47.AchievementPlanner.ViewModel
     public ObservableCollection<IGame> Games
     {
       get { return _games; }
+    }
+
+    public ICommand UpdateGameCommand
+    {
+      get;
     }
   }
 }
